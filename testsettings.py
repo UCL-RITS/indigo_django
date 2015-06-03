b07f1fbfa0ffa64ef28a526f11a9aafc5536ca37
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2015-06-03 12:20:42 +0100

    Add testsettings

---
+++ testsettings.py
@@ -0,0 +1,16 @@
+SECRET_KEY = 'xxxxxx'
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+INSTALLED_APPS = (
+    'indigo',
+)
