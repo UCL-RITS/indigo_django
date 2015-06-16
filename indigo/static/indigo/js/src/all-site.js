f5e8580d4a8b179b17f2566ad451c5f939aa3e02
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2015-06-16 12:07:33 +0100

    Update to latest Indigo

--- indigo/static/indigo/js/src/all-site.js
+++ indigo/static/indigo/js/src/all-site.js
@@ -225,18 +225,98 @@ gs = {
 };
 
 gs.init();
+/* lazyload.js (c) Lorenzo Giuliani
+ * MIT License (http://www.opensource.org/licenses/mit-license.html)
+ *
+ * expects a list of:  
+ * `<img src="blank.gif" data-src="my_image.png" width="600" height="400" class="lazy">`
+ */
+
+!function(window){
+	var $q = function(q, res){
+				if (document.querySelectorAll) {
+					res = document.querySelectorAll(q);
+				} else {
+					var d=document
+						, a=d.styleSheets[0] || d.createStyleSheet();
+					a.addRule(q,'f:b');
+					for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+						l[b].currentStyle.f && c.push(l[b]);
+
+					a.removeRule(0);
+					res = c;
+				}
+				return res;
+			}
+		, addEventListener = function(evt, fn){
+				window.addEventListener
+					? this.addEventListener(evt, fn, false)
+					: (window.attachEvent)
+						? this.attachEvent('on' + evt, fn)
+						: this['on' + evt] = fn;
+			}
+		, _has = function(obj, key) {
+				return Object.prototype.hasOwnProperty.call(obj, key);
+			}
+		;
+
+	function loadImage (el, fn) {
+		var img = new Image()
+			, src = el.getAttribute('data-src');
+		img.onload = function() {
+			if (!! el.parent)
+				el.parent.replaceChild(img, el)
+			else
+				el.src = src;
+
+			fn? fn() : null;
+		}
+		img.src = src;
+	}
+
+	function elementInViewport(el) {
+		var rect = el.getBoundingClientRect()
+
+		return (
+			 rect.top    >= 0
+		&& rect.left   >= 0
+		&& rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+		)
+	}
+
+		var images = new Array()
+			, query = $q('img.lazy')
+			, processScroll = function(){
+					for (var i = 0; i < images.length; i++) {
+						if (elementInViewport(images[i])) {
+							loadImage(images[i], function () {
+								images.splice(i, i);
+							});
+						}
+					};
+				}
+			;
+		// Array.prototype.slice.call is not callable under our lovely IE8 
+		for (var i = 0; i < query.length; i++) {
+			images.push(query[i]);
+		};
+
+		processScroll();
+		addEventListener('scroll',processScroll);
+
+}(this);​
 // UCL JS
 $(document).ready(function() {
 
-	$('.tabbed .tabbed__content').hide();
-	$('.tabbed .tabbed__content:first').show();
-	$('.tabbed .tabbed__item:first').addClass('is-active');
+	$('.tabbed div').hide();
+	$('.tabbed div:first').show();
+	$('.tabbed ul li:first').addClass('is-active');
 
-	$('.tabbed .tabbed__link').click(function() {
-		$('.tabbed .tabbed__item').removeClass('is-active');
+	$('.tabbed ul li a').click(function() {
+		$('.tabbed ul li').removeClass('is-active');
 		$(this).parent().addClass('is-active');
 		var currentTab = $(this).attr('href');
-		$('.tabbed .tabbed__content').hide();
+		$('.tabbed div').hide();
 		$(currentTab).show();
 		return false;
 	});
@@ -282,7 +362,7 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 
-	if (document.documentElement.clientWidth < 767) {
+	if (Modernizr.mq('only screen and (max-width: 768px)')) {
 		//Add Inactive Class To All Accordion Headers
 		$('.collapse__header').addClass('collapse__header--inactive');
 
@@ -306,5 +386,7 @@ $(document).ready(function() {
 				$(this).next().slideToggle().toggleClass('open-content');
 			}
 		});
+	}else{
+		$('.collapse__header').addClass('collapse__header--active');
 	}
 });
