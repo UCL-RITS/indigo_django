3bc44036db113ad054275517702c4c1f5ec07345
Helen Sherwood-Taylor <helen@rrdlabs.co.uk>
2015-06-23 17:17:20 +0100

    Version 0.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='indigo-django',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     install_requires=['Django>=1.8'],
     include_package_data=True,
